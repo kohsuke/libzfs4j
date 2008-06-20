e11326b8d00eeaea9274029b3fffc710af17ab7b
kohsuke <null>
2008-06-20 01:20:43 +0000

    [maven-release-plugin]  copy for tag libzfs-0.1

---
+++ src/main/java/org/jvnet/solaris/libzfs/jna/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Low level API to <tt>libzfs</tt> through JNA. Not meant to be invoked directly for typical use.
+ */
+package org.jvnet.solaris.libzfs.jna;
---
+++ src/main/java/org/jvnet/solaris/libzfs/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * High-level Java API to <tt>libzfs</tt>. Start navigating from {@link LibZFS}.
+ */
+package org.jvnet.solaris.libzfs;
