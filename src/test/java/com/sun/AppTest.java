6a1153bfbc74ef24454610c5dcd004404e567354
kohsuke <null>
2009-02-12 18:19:39 +0000

    this seems to cause segfault

--- src/test/java/com/sun/AppTest.java
+++ src/test/java/com/sun/AppTest.java
@@ -202,6 +202,7 @@ public class AppTest extends TestCase {
         assertEquals(time,v);
 
         o2.inheritProperty(property);
+        o2.dispose();
         o2 = zfs.open(dataSet+"/child",ZFSFileSystem.class);
 
         v = o2.getUserProperty(property);
