0ee4535232a0ade27fef6f29f1b9c2f1bbbf470b
Jim Klimov <jim@jimklimov.com>
2017-04-23 00:55:38 +0200

    lockpick-libzfs-abi.sh : final rearrangement of code

--- lockpick-libzfs-abi.sh
+++ lockpick-libzfs-abi.sh
@@ -23,6 +23,44 @@ ulimit -c 0
 
 [ -n "${VERBOSITY-}" ] || VERBOSITY=quiet
 
+# Put the variants with longer argument lists or otherwise more-probable
+# to fail first (newer ABIs usually), to reduce amount of false-positive
+# identifications. In many cases, the legacy set of arguments fits inside
+# newer argument list and so does not cause a linking error, but still can
+# potentially pass random heap garbage to the actually called new function.
+# Find the options scaterred in sources (mostly ZFSObject.java).
+declare -A LIBZFS_VARIANT_FUNCTIONS
+
+LIBZFS_VARIANT_FUNCTIONS["zfs_snapshot"]="openzfs legacy pre-nv96"
+LIBZFS_VARIANT_FUNCTIONS["zfs_iter_snapshots"]="openzfs legacy"
+LIBZFS_VARIANT_FUNCTIONS["zfs_destroy_snaps"]="openzfs legacy"
+LIBZFS_VARIANT_FUNCTIONS["zfs_destroy"]="openzfs legacy"
+
+# TODO: New ABI syntax for either major branch of ZFS has not yet been
+# figured out, so routines are sort of deprecated (NO-OPs) until then.
+# Still, to allow yet older Solarises to run well, you can want to test
+# the old ABI first -- be sure to use TEST_OK_ZERO_ONLY=yes as well.
+# By currently more probable default we just "no-op" them, although
+# the "legacy" or "openzfs" options are also valid until implemented.
+#LIBZFS_VARIANT_FUNCTIONS["zfs_perm_remove"]="pre-sol10u8 NO-OP"
+#LIBZFS_VARIANT_FUNCTIONS["zfs_perm_set"]="pre-sol10u8 NO-OP"
+LIBZFS_VARIANT_FUNCTIONS["zfs_perm_remove"]="NO-OP pre-sol10u8"
+LIBZFS_VARIANT_FUNCTIONS["zfs_perm_set"]="NO-OP pre-sol10u8"
+
+
+### NOTE: Better just create that dataset and `zfs allow` your account to test
+### in it. Or use
+### "$(mkfile -v 16M testpool.img && zpool create testpool `pwd`/testpool.img)"
+### to create a test pool (as root)...
+###   sudo zfs allow -ld jim mount,create,share,destroy,snapshot rpool/kohsuke
+#LIBZFSTEST_DATASET="rpool/kohsuke"
+LIBZFSTEST_DATASET="missingrpool/fakedataset"
+LIBZFSTEST_MVN_OPTIONS="${LIBZFSTEST_MVN_OPTIONS-} -Dlibzfs.test.pool=${LIBZFSTEST_DATASET}"
+LIBZFSTEST_MVN_OPTIONS="${LIBZFSTEST_MVN_OPTIONS-} -Dlibzfs.test.loglevel=FINEST"
+### Avoid parallel test-cases inside our class - it is unreadable to debug
+LIBZFSTEST_MVN_OPTIONS="${LIBZFSTEST_MVN_OPTIONS-} -Dparallel=classes -DforkCount=0"
+
+
 die() {
     RES="$1"
     [ -n "$RES" ] && [ "$RES" -gt 0 ] && shift || RES=1
@@ -32,22 +70,18 @@ die() {
     exit $RES
 }
 
+report_match() {
+    SETTINGS="$(set | egrep '^LIBZFS4J_.*=')"
+    uname -a
+    echo "MATCHED with the following settings: " $SETTINGS
+    return 0
+}
+
 build_libzfs() {
     echo "Building latest libzfs4j.jar and tests..."
     mvn compile test-compile 2>/dev/null >/dev/null && echo "OK" || \
         die $? "FAILED to build code"
 }
-
-### NOTE: Better just create that dataset and `zfs allow` your account to test
-### in it. Or use
-### "$(mkfile -v 16M testpool.img && zpool create testpool `pwd`/testpool.img)"
-### to create a test pool (as root)...
-###   sudo zfs allow -ld jim mount,create,share,destroy,snapshot rpool/kohsuke
-LIBZFSTEST_DATASET="rpool/kohsuke"
-LIBZFSTEST_MVN_OPTIONS="${LIBZFSTEST_MVN_OPTIONS-} -Dlibzfs.test.pool=${LIBZFSTEST_DATASET}"
-LIBZFSTEST_MVN_OPTIONS="${LIBZFSTEST_MVN_OPTIONS-} -Dlibzfs.test.loglevel=FINEST"
-### Avoid parallel test-cases inside our class - it is unreadable to debug
-LIBZFSTEST_MVN_OPTIONS="${LIBZFSTEST_MVN_OPTIONS-} -Dparallel=classes -DforkCount=0"
 
 test_libzfs() (
     echo ""
@@ -62,8 +96,8 @@ test_libzfs() (
     OUT="$(mvn -DargLine="${DUMPING_OPTS}" $LIBZFSTEST_MVN_OPTIONS "$@" test 2>&1)" || RES=$?
     case "$VERBOSITY" in
     high)
-        echo "$OUT" | egrep '^FINE.*LIBZFS4J' | uniq
-        echo "$OUT" | egrep -v '^FINE.*LIBZFS4J|org.jvnet.solaris.libzfs.LibZFS initFeatures|^$'
+        echo "$OUT" | egrep '^FINE.*libzfs4j features.*LIBZFS4J' | uniq
+        echo "$OUT" | egrep -v '^FINE.*libzfs4j features.*LIBZFS4J|org.jvnet.solaris.libzfs.LibZFS initFeatures|^$'
         ;;
     yes)
         echo "$OUT" | egrep '^FINE.*LIBZFS4J' | uniq
@@ -74,6 +108,7 @@ test_libzfs() (
 
     case "$RES" in
         0|1) # Test could fail e.g. due to inaccessible datasets
+             # or permissions. Normally we do not bail on this.
             echo "SUCCESS (did not core-dump, returned $RES)"
             if [ "$TEST_OK_ZERO_ONLY" = yes ]; then
                 return $RES
@@ -109,24 +144,38 @@ test_all_routines() {
     test_libzfs -Dlibzfs.test.funcname="${!LIBZFS_VARIANT_FUNCTIONS[*]}"
 }
 
-# Put most-probable variants first, to reduce amount of iterations
-declare -A LIBZFS_VARIANT_FUNCTIONS
-LIBZFS_VARIANT_FUNCTIONS["zfs_iter_snapshots"]=" legacy openzfs"
-#LIBZFS_VARIANT_FUNCTIONS["zfs_iter_snapshots"]="openzfs legacy"
-### Do not normally test pre-nv96 early on - it fits inside newer
-### argument list and so does not cause a linking error, but can
-### potentially pass random heap garbage to the called new signature.
-###LIBZFS_VARIANT_FUNCTIONS["zfs_snapshot"]="pre-nv96 legacy openzfs"
-LIBZFS_VARIANT_FUNCTIONS["zfs_snapshot"]="openzfs legacy pre-nv96"
-LIBZFS_VARIANT_FUNCTIONS["zfs_destroy_snaps"]="openzfs legacy"
-LIBZFS_VARIANT_FUNCTIONS["zfs_destroy"]="openzfs legacy"
-# TODO: New ABI syntax for either major branch of ZFS has not yet been
-# figured out, so routines are sort of deprecated (NO-OPs) until then.
-# Still, to allow yet older Solarises to run well, we test the old ABI first.
-LIBZFS_VARIANT_FUNCTIONS["zfs_perm_remove"]="pre-sol10u8 NO-OP"
-LIBZFS_VARIANT_FUNCTIONS["zfs_perm_set"]="pre-sol10u8 NO-OP"
 
-#LIBZFS4J_ABI_zfs_destroy_snaps=openzfs, LIBZFS4J_ABI_zfs_iter_snapshots=openzfs, LIBZFS4J_ABI_zfs_destroy=openzfs, LIBZFS4J_ABI_zfs_perm_remove=NO-OP, LIBZFS4J_ABI_zfs_perm_set=NO-OP, LIBZFS4J_ABI_zfs_snapshot=openzfs, LIBZFS4J_ABI=openzfs
+test_lockpick() {
+    # Override the default for individual variants explicitly in the loop below
+    LIBZFS4J_ABI=legacy
+    echo ""
+    echo "Simple approach failed - begin lockpicking..."
+    for ZFS_FUNCNAME in "${!LIBZFS_VARIANT_FUNCTIONS[@]}" ; do
+        eval LIBZFS4J_ABI_${ZFS_FUNCNAME}="NO-OP"
+        eval export LIBZFS4J_ABI_${ZFS_FUNCNAME}
+    done
+
+    for ZFS_FUNCNAME in "${!LIBZFS_VARIANT_FUNCTIONS[@]}" ; do
+        echo ""
+        # Note: Empty token must be in the end - for library picking defaults,
+        # and as the fatal end of loop if nothing tried works for this system.
+        for ZFS_VARIANT in ${LIBZFS_VARIANT_FUNCTIONS[${ZFS_FUNCNAME}]} "" ; do
+            eval LIBZFS4J_ABI_${ZFS_FUNCNAME}="${ZFS_VARIANT}"
+            eval export LIBZFS4J_ABI_${ZFS_FUNCNAME}
+            echo "Testing function variant LIBZFS4J_ABI_${ZFS_FUNCNAME}='${ZFS_VARIANT}'..."
+            test_libzfs -Dlibzfs.test.funcname="${ZFS_FUNCNAME}" -X && break
+            if [ -z "$ZFS_VARIANT" ]; then
+                die 1 "FAILED to find a working variant for $ZFS_FUNCNAME"
+            fi
+            #die 1 "After first test"
+        done
+        echo "============== Picked function variant LIBZFS4J_ABI_${ZFS_FUNCNAME}='${ZFS_VARIANT}'..."
+        #die 1 "After one loop"
+    done
+
+    # if here, we found variants for all functions
+    return 0
+}
 
 ##################### DO THE WORK #########################
 
@@ -137,34 +186,9 @@ export LIBZFS4J_ABI
 
 test_linkability
 
-#test_defaults && test_all_routines && exit
+test_defaults && test_all_routines && report_match && exit
 
-# Override the default for individual variants explicitly in the loop below
-LIBZFS4J_ABI=legacy
-echo ""
-echo "Simple approach failed - begin lockpicking..."
-for ZFS_FUNCNAME in "${!LIBZFS_VARIANT_FUNCTIONS[@]}" ; do
-    eval LIBZFS4J_ABI_${ZFS_FUNCNAME}="NO-OP"
-    eval export LIBZFS4J_ABI_${ZFS_FUNCNAME}
-done
-
-for ZFS_FUNCNAME in "${!LIBZFS_VARIANT_FUNCTIONS[@]}" ; do
-    echo ""
-    # Note: Empty token must be in the end - for library picking defaults,
-    # and as the fatal end of loop if nothing tried works for this system.
-    for ZFS_VARIANT in ${LIBZFS_VARIANT_FUNCTIONS[${ZFS_FUNCNAME}]} "" ; do
-        eval LIBZFS4J_ABI_${ZFS_FUNCNAME}="${ZFS_VARIANT}"
-        eval export LIBZFS4J_ABI_${ZFS_FUNCNAME}
-        echo "Testing function variant LIBZFS4J_ABI_${ZFS_FUNCNAME}='${ZFS_VARIANT}'..."
-        test_libzfs -Dlibzfs.test.funcname="${ZFS_FUNCNAME}" -X && break
-        if [ -z "$ZFS_VARIANT" ]; then
-            die 1 "FAILED to find a working variant for $ZFS_FUNCNAME"
-        fi
-        #die 1 "After first test"
-    done
-    echo "============== Picked function variant LIBZFS4J_ABI_${ZFS_FUNCNAME}='${ZFS_VARIANT}'..."
-    #die 1 "After one loop"
-done
+test_lockpick
 
 echo ""
 echo "Re-validating the full set of lockpicking results..."
@@ -174,4 +198,5 @@ echo ""
 echo "Packaging the results..."
 mvn $LIBZFSTEST_MVN_OPTIONS package || die $? "FAILED packaging"
 
+report_match
 exit 0
